d1edf0fd51e4a688911f833e0b7bf902d9684960
Dustin <lambright.dustin@gmail.com>
2016-11-13 13:33:23 -0500

    Initial Commit

---
+++ EZRecipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EZRecipe
+//
+//  Created by Charlie Buckets on 11/13/16.
+//  Copyright © 2016 ChalieBuckets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
